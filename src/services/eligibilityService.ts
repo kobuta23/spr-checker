6269f3ed3221c240a5ee43d09346da7fe382af19
kobuta23 <gadousinsin@gmail.com>
2025-04-02 04:26:16 +0900

    fix build

--- src/services/eligibilityService.ts
+++ src/services/eligibilityService.ts
@@ -3,7 +3,7 @@ import blockchainService from './blockchain/blockchainService';
 import logger from '../utils/logger';
 import config from '../config';
 import { AddressEligibility, PointSystemEligibility, StackAllocation } from '../models/types';
-import { checkRecipientsToppedUp, latestRecipients } from '../utils/UBARecipients';
+import { latestRecipients } from '../utils/UBARecipients';
 const { POINT_THRESHOLD, POINTS_TO_ASSIGN, COMMUNITY_ACTIVATION_ID, THRESHOLD_TIME_PERIOD, THRESHOLD_MAX_USERS } = config;
 class EligibilityService {
   /**
