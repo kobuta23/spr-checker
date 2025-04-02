ea1df1baa3281056d8ffe747dea8a431d7810877
kobuta23 <gadousinsin@gmail.com>
2025-04-02 13:11:14 +0900

    moved cache to 20min

--- src/utils/UBARecipients.ts
+++ src/utils/UBARecipients.ts
@@ -134,7 +134,7 @@ export const getRecipient = (address: string): UniversalPointRecipient | null =>
  */
 export const checkRecipients = async (): Promise<void> => {
   const recipients = getStoredRecipients();
-  const recipientsToCheck = recipients.filter(r => (!r.lockerAddress || !r.claimed) && (!r.lastChecked || new Date(r.lastChecked) < new Date(Date.now() - 1000 * 60)));
+  const recipientsToCheck = recipients.filter(r => (!r.lockerAddress || !r.claimed) && (!r.lastChecked || new Date(r.lastChecked) < new Date(Date.now() - 1000 * 60 * 20))); // 20 minute cache
   const recipientAddressList = recipientsToCheck.map(r => r.address);
   const lockerAddresses = await blockchainService.getLockerAddresses(recipientAddressList);
   const claimStatuses = await blockchainService.checkAllClaimStatuses(lockerAddresses);
